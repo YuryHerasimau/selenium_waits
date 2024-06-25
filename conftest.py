8c72905537f5de8cf5c45edaf9065b0618eb1117
Yury Herasimau <yl.herasimau@gmail.com>
2024-06-25 17:32:56 +0300

    Some fixes

--- conftest.py
+++ conftest.py
@@ -16,6 +16,11 @@ def chrome_options():
     options = Options()
     # options.add_argument("--headless")
     options.add_argument("--start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
     return options
 
 
