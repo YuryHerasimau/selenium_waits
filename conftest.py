7f570f895433bb54cca5f7c03e0f934013af3f67
Yury Herasimau <yl.herasimau@gmail.com>
2024-06-25 18:12:52 +0300

    Some fixes

--- conftest.py
+++ conftest.py
@@ -21,6 +21,7 @@ def chrome_options():
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
+    options.add_argument('--remote-debugging-pipe')
     return options
 
 
